2ea187e53736f2238be915bbbde779f5227452a7
TenoraoBr <44686096+TenoraoBr@users.noreply.github.com>
2019-06-05 19:50:26 -0300

    Adicionei o arquivo Hello.java

---
+++ Hello.java
@@ -0,0 +1,13 @@
+/**
+* Hello World Java 
+* @author Tenório
+*/
+
+public class Hello{
+	//método principal
+	public static void main(String[] args){
+		System.out.println("Hello World");
+		
+		
+	}
+}
